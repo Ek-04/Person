a96259893a4c5cee77cca7a84b0b6164fa13b2a4
Ek-04 <125458901+Ek-04@users.noreply.github.com>
2024-04-10 10:26:59 +0300

    Update class.py

--- class.py
+++ class.py
@@ -3,10 +3,13 @@ class Person: #create class person
         #ask the user to enter name and age
         self.name = input("Eter your name here:")
         self.age = int(input("Enter youtr age here:"))
+        self.gender = input("Enter your gender here:")
+
 
     def introduce (self):
         print("Name is:", self.name)
         print("Age is:", self.age)
+        print("Gender is:", self.gender)
     #create instace person1 and call th method introduce 
 person1 = Person ()
 person1.introduce()
